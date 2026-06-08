04a0b4a934e4164b14e3737b84574d172475763a
K Sneha <sneha27092004@gmail.com>
2026-06-08 10:01:23 +0530

    Add main class to connect to the database

---
+++ src/main.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+
+import database.DBConnection;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Connection con =
+                DBConnection.getConnection();
+
+        if(con != null) {
+
+            System.out.println(
+                    "Database Connected Successfully");
+
+        } else {
+
+            System.out.println(
+                    "Connection Failed");
+        }
+    }
+}
